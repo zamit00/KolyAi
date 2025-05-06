aa5760924bdd47da71bf9de7d68e5a3011828349
zamit00 <zamit00@gmail.com>
2025-05-06 06:48:31 +0300

    Update hasifot.js

--- hasifot.js
+++ hasifot.js
@@ -188,8 +188,6 @@ function createForm(x) {
     const filter=document.getElementById('filter');
     filter.style.display='flex';
     document.getElementById("productForm").scrollIntoView({ behavior: "smooth" });
-if(document.getElementById("menu").classList.includes
-   ('open')){document.getElementById("menu").classList.remove("open")};
 if(x!==0){
     tablhasifot()
   }
